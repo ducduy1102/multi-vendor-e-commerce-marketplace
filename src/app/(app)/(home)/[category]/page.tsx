13cef64c2691054f11f5d3fd66afbaa826bdb6ff
ducduy1102 <ducduy1110u@gmail.com>
2025-09-28 09:57:25 +0700

    Fix: fix dropdown menu position getting lost, incorrect prefetching for infinite queries

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -2,6 +2,7 @@ import type { SearchParams } from "nuqs/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { getQueryClient, trpc } from "@/trpc/server";
+import { DEFAULT_LIMIT } from "@/constants";
 
 import { loadProductFilters } from "@/modules/products/search-params";
 import { ProductListView } from "@/modules/products/ui/views/product-list-view";
@@ -15,8 +16,12 @@ const Page = async ({ params, searchParams }: Props) => {
   const filters = await loadProductFilters(searchParams);
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.products.getMany.queryOptions({ category, ...filters })
+  void queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({ 
+      ...filters ,
+      category, 
+      limit: DEFAULT_LIMIT
+    })
   );
 
   return (
