7686f385a502fb3a1ad4fecf737153f9fabcb4a6
ducduy1102 <ducduy1110u@gmail.com>
2025-12-31 02:15:09 +0700

    Feat: access control

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -8,6 +8,8 @@ import { buildConfig } from "payload";
 import { fileURLToPath } from "url";
 import sharp from "sharp";
 
+import { isSuperAdmin } from "@/lib/acccess";
+
 import { Users } from "./collections/Users";
 import { Media } from "./collections/Media";
 import { Categories } from "./collections/Categories";
@@ -57,8 +59,7 @@ export default buildConfig({
       tenantsArrayField: {
         includeDefaultField: false,
       },
-      userHasAccessToAllTenants: (user) =>
-        Boolean(user?.roles?.includes("super-admin")),
+      userHasAccessToAllTenants: (user) => isSuperAdmin(user),
     }),
     // storage-adapter-placeholder
   ],
